8af97f43473a484c4def14798d51fcbd8afbc19f
dk949 <56653556+dk949@users.noreply.github.com>
2022-02-07 12:44:22 +0000

    parser: ref: split parser into source and header

---
+++ src/parser/parser.cpp
@@ -0,0 +1,79 @@
+#include "parser.hpp"
+
+Parser::Parser(Vector<Token> tokens)
+        : m_tokens(std::move(tokens))
+        , m_current(m_tokens.begin()) { }
+
+
+Ast::NodePtr Parser::makeNode() {
+    if (m_current->type() == TokenType::T_KEYWORD && m_current->get<Token::Keyword>() == Token::DEF) {
+        return makeFunction();
+    }
+    todo();
+}
+
+Ast::FunctionDeclPtr Parser::makeFunction() {
+    m_current = std::next(m_current);
+
+    if (m_current->type() == TokenType::T_KEYWORD && m_current->get<Token::Keyword>() == Token::MAIN) {
+        m_current = std::next(m_current);
+
+
+        // Ignored, as long as it exists on entry point
+        // makes you wander why we need it on the enty point
+        makeTypeAnnotation();
+
+        verify(isScopeBegin());
+
+        auto block = MakePtr<Ast::Block>();
+
+        m_current = std::next(m_current);
+        while (!isScopeEnd()) {
+            verify(!isLast());
+            block->append(makeNode());
+            m_current = std::next(m_current);
+        }
+        auto func = MakePtr<Ast::Entry>(block);
+    }
+    todo();
+}
+
+Ast::ValueType Parser::makeTypeAnnotation() {
+    if (!isLast() && m_current->type() == TokenType::T_OP && m_current->get<Token::Operator>() == Token::COLON) {
+        m_current = std::next(m_current);
+        if (!isLast() && m_current->type() == TokenType::T_BUILTIN_TYPE) {
+            switch (m_current->get<Token::BuiltinType>()) {
+                bcase Token::INT : {
+                    m_current = std::next(m_current);
+                    return Ast::ValueType::INT;
+                }
+                BAD_ENUM_CASE(Token::TYPE_COUNT);
+            }
+        }
+        // handle user defined types
+        todo();
+    }
+    // handle implicit types
+    todo();
+}
+
+
+bool Parser::isLast() {
+    return isLast(m_current);
+}
+
+bool Parser::isLast(Iter i) {
+    return i != m_tokens.end();
+}
+
+bool Parser::isReturn() {
+    return m_current->type() == TokenType::T_KEYWORD && m_current->get<Token::Keyword>() == Token::RETURN;
+}
+
+bool Parser::isScopeBegin() {
+    return m_current->type() == TokenType::T_OP && m_current->get<Token::Operator>() == Token::LCURLY;
+}
+
+bool Parser::isScopeEnd() {
+    return m_current->type() == TokenType::T_OP && m_current->get<Token::Operator>() == Token::RCURLY;
+}
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -9,81 +9,22 @@ class Parser {
     Vector<Token> m_tokens;
     Iter m_current;
 public:
-    Parser(Vector<Token> tokens)
-            : m_tokens(std::move(tokens))
-            , m_current(m_tokens.begin()) { }
+    explicit Parser(Vector<Token> tokens);
 
-    Ast::NodePtr makeNode() {
-        if (m_current->type() == TokenType::T_KEYWORD && m_current->get<Token::Keyword>() == Token::DEF) {
-            return makeFunction();
-        }
-        todo();
-    }
+    Ast::NodePtr makeNode();
+    Ast::FunctionDeclPtr makeFunction();
 
-    Ast::FunctionDeclPtr makeFunction() {
-        m_current = std::next(m_current);
+    Ast::ValueType makeTypeAnnotation();
 
-        if (m_current->type() == TokenType::T_KEYWORD && m_current->get<Token::Keyword>() == Token::MAIN) {
-            m_current = std::next(m_current);
+    bool isLast();
 
+    bool isLast(Iter i);
 
-            // Ignored, as long as it exists on entry point
-            // makes you wander why we need it on the enty point
-            makeTypeAnnotation();
+    bool isReturn();
 
-            verify(isScopeBegin());
+    bool isScopeBegin();
 
-            auto block = MakePtr<Ast::Block>();
-
-            m_current = std::next(m_current);
-            while (!isScopeEnd()) {
-                verify(!isLast());
-                block->append(makeNode());
-                m_current = std::next(m_current);
-            }
-            auto func = MakePtr<Ast::Entry>(block);
-        }
-        todo();
-    }
-
-    Ast::ValueType makeTypeAnnotation() {
-        if (!isLast() && m_current->type() == TokenType::T_OP && m_current->get<Token::Operator>() == Token::COLON) {
-            m_current = std::next(m_current);
-            if (!isLast() && m_current->type() == TokenType::T_BUILTIN_TYPE) {
-                switch (m_current->get<Token::BuiltinType>()) {
-                    bcase Token::INT : {
-                        m_current = std::next(m_current);
-                        return Ast::ValueType::INT;
-                    }
-                    BAD_ENUM_CASE(Token::TYPE_COUNT);
-                }
-            }
-            // handle user defined types
-            todo();
-        }
-        // handle implicit types
-        todo();
-    }
-
-    bool isLast() {
-        return isLast(m_current);
-    }
-
-    bool isLast(Iter i) {
-        return i != m_tokens.end();
-    }
-
-    bool isReturn() {
-        return m_current->type() == TokenType::T_KEYWORD && m_current->get<Token::Keyword>() == Token::RETURN;
-    }
-
-    bool isScopeBegin() {
-        return m_current->type() == TokenType::T_OP && m_current->get<Token::Operator>() == Token::LCURLY;
-    }
-
-    bool isScopeEnd() {
-        return m_current->type() == TokenType::T_OP && m_current->get<Token::Operator>() == Token::RCURLY;
-    }
+    bool isScopeEnd();
 };
 
 #endif  // PARSER_HPP
