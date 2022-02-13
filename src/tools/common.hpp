dea8f1d991780b761daa8900e1e019b4bdccfd5d
dk949 <56653556+dk949@users.noreply.github.com>
2022-02-13 23:26:37 +0000

    tools: fix: `to()` no longer copies the argument

--- src/tools/common.hpp
+++ src/tools/common.hpp
@@ -1,6 +1,7 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 #include "types.hpp"
+#include <utility>
 
 namespace Tools {
 template<typename>
@@ -16,9 +17,9 @@ constexpr std::string_view shortEnumName(std::string_view sv) {
 }
 }
 
-template<typename T>
-constexpr T to(auto o) {
-    return static_cast<T>(o);
+template<typename T, typename U>
+constexpr T to(U &&o) {
+    return static_cast<T>(std::forward<U>(o));
 }
 template<typename T>
 constexpr T is(auto *o) {
@@ -29,8 +30,8 @@ constexpr T *is(Ptr<U> o) {
     return dynamic_cast<T *>(o.get());
 }
 template<typename T>
-constexpr T as(auto o) {
-    return reinterpret_cast<T>(o);
+constexpr T *as(auto *o) {
+    return reinterpret_cast<T *>(o);
 }
 
 #define dev [[maybe_unused]]
