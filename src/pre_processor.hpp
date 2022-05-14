581f5426133441038d4335db3d68e59c05d5171b
dk949 <56653556+dk949@users.noreply.github.com>
2022-05-14 01:45:45 +0100

    comp: added preprocessor base class

---
+++ src/pre_processor.hpp
@@ -0,0 +1,16 @@
+#ifndef PRE_PROCESSOR_HPP
+#define PRE_PROCESSOR_HPP
+
+#include "ast_fwd.hpp"
+#include "error.hpp"
+
+
+class Preprocessor {
+public:
+    [[nodiscard]] virtual ErrorOr<Program> preprocess(Program &&) const = 0;
+    [[nodiscard]] virtual constexpr std::string_view preprocessorType() const = 0;
+
+    virtual ~Preprocessor() = default;
+};
+
+#endif  // PRE_PROCESSOR_HPP
