99b95e5d801cac7bebc03f213985236425f5e91a
dk949 <56653556+dk949@users.noreply.github.com>
2022-05-14 01:46:23 +0100

    io: added output base class

---
+++ src/output.hpp
@@ -0,0 +1,13 @@
+#ifndef OUTPUT_HPP
+#define OUTPUT_HPP
+
+#include <string>
+
+class Output {
+public:
+    virtual void output(std::string &&) const = 0;
+    [[nodiscard]] virtual constexpr std::string_view outputType() const = 0;
+    virtual ~Output() = default;
+};
+
+#endif  // OUTPUT_HPP
