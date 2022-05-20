f44770297ae882a6930606a318e4390ac956d6b6
dk949 <56653556+dk949@users.noreply.github.com>
2022-05-20 08:24:01 +0100

    test: added driver for all the test files

--- test/test_driver.cpp
+++ test/test_driver.cpp
@@ -1,8 +1,79 @@
+#include "common.hpp"
+#include "empty_post_proc.hpp"
+#include "empty_pre_proc.hpp"
+#include "parser_interface.hpp"
+#include "parser_options.hpp"
+#include "pipeline.hpp"
+#include "test_io.hpp"
+#include "wasm_compiler.hpp"
+
 #include <catch2/catch.hpp>
 #include <filesystem>
 #include <fstream>
+#include <iostream>
+#include <memory>
+#include <project/config.hpp>
+#include <vector>
 namespace fs = std::filesystem;
 
 
-TEST_CASE("const declarations", "[declarations]") {
+Pipeline makeTestPipeline(std::string &out, Error &err) {
+    return Pipeline {//
+        scotch::makeVector<std::unique_ptr<Preprocessor>>(std::make_unique<EmptyPreproc>()),
+        std::make_unique<WasmCompiler>(),
+        scotch::makeVector<std::unique_ptr<Postprocessor>>(std::make_unique<EmptyPostproc>()),
+        std::make_unique<TestOutput>(&out),
+        std::make_unique<TestError>(&err)};
+}
+
+
+using TestFiles = std::vector<std::pair<fs::path, std::ifstream>>;
+
+TestFiles openTestFiles(fs::path testdir) {
+    const fs::directory_iterator dirContents {fs::path(scotch::project::dirs::test) / testdir};
+    std::vector<std::pair<fs::path, std::ifstream>> vec;
+    std::transform(fs::begin(dirContents), fs::end(dirContents), std::back_inserter(vec), [](const auto &dirent) {
+        auto path = fs::path {dirent};
+        auto file = std::ifstream {path};
+        if (!file) {
+            throw std::runtime_error {"file " + path.string() + " does not exist"};
+        }
+        return std::make_pair(std::move(path), std::move(file));
+    });
+
+    return vec;
+}
+
+void runTests(TestFiles &&testFiles) {
+    for (auto &[path, i] : testFiles) {
+        INFO("Test " << path << '\n');
+        std::string out;
+        Error err;
+
+        ParserOptions::clearPipeline();
+        ParserOptions::setPipeline(makeTestPipeline(out, err));
+        ParserInterface parser {&i};
+        int res = parser.parse();
+        if (path.string().find("error") != out.npos) {
+            CHECK(res != 0);
+            CHECK(!err.msg.empty());
+        } else {
+            CHECK(res == 0);
+            CHECK(err.msg.empty());
+        }
+    }
+}
+
+
+TEST_CASE("declarations") {
+    runTests(openTestFiles("declaration_tests"));
+}
+
+TEST_CASE("functions") {
+    runTests(openTestFiles("function_tests"));
+}
+
+TEST_CASE("parameters") {
+    TestFiles testFiles = openTestFiles("parameter_tests");
+    runTests(openTestFiles("parameter_tests"));
 }
