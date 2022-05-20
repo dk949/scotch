7df216e19be9e1fe672c41958509db7e74f8a47f
dk949 <56653556+dk949@users.noreply.github.com>
2022-05-20 08:24:47 +0100

    main: moved parser related logic from main, to help testability

--- src/main.cpp
+++ src/main.cpp
@@ -6,46 +6,20 @@
 #include "console_out.hpp"
 #include "empty_post_proc.hpp"
 #include "empty_pre_proc.hpp"
+#include "parser_interface.hpp"
 #include "parser_options.hpp"
 #include "wasm_compiler.hpp"
 #include "wasm_formatter.hpp"
 
 #include <cstdlib>
-#include <FlexLexer.h>
 #include <fmt/format.h>
 #include <fstream>
 #include <iostream>
 #include <numeric>
 #include <optional>
-#include <parser.hpp>
 #include <vector>
 
 std::optional<yyFlexLexer> lexer;
-
-int yylex(scotch::parser::semantic_type *yylval, scotch::parser::location_type *yylloc) {
-    yylloc->begin.line = lexer->lineno();
-    int token = lexer->yylex();
-    switch (token) {
-        case scotch::parser::token::IDENT:
-            yylval->build(std::string(lexer->YYText()));
-            break;
-        case scotch::parser::token::Int32:
-        case scotch::parser::token::Int64:
-            yylval->build(std::strtoll(lexer->YYText(), nullptr, 10));
-            break;
-        case scotch::parser::token::Float32:
-        case scotch::parser::token::Float64:
-            yylval->build(std::strtod(lexer->YYText(), nullptr));
-            break;
-    }
-    return token;
-}
-
-void scotch::parser::error(const location_type &loc, const std::string &msg) {
-    std::cerr << loc.begin.line << ':' << loc.begin.column << ':' << msg << '\n';
-    exit(1);
-}
-
 
 
 int main(int, char *argv[]) {
@@ -70,9 +44,6 @@ int main(int, char *argv[]) {
         std::make_unique<ConsoleError>()};
 
     ParserOptions::setPipeline(std::move(p));
-
-    lexer.emplace(&input);
-    scotch::parser parser;
-    parser.parse();
-    return 0;
+    ParserInterface parser {&input};
+    return parser.parse();
 }
