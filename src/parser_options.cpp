e899b2f371e6b94dea554de2e92f22d0b2e78c27
dk949 <56653556+dk949@users.noreply.github.com>
2022-06-01 09:13:55 +0300

    parser: fixed use of optional in parser_options

--- src/parser_options.cpp
+++ src/parser_options.cpp
@@ -10,18 +10,18 @@ void ParserOptions::setPipeline(Pipeline &&a) {
     m_pipeline.emplace(std::move(a));
 }
 
-std::optional<Pipeline> ParserOptions::m_pipeline = std::nullopt;
+std::optional<Pipeline> ParserOptions::m_pipeline{};
 
 Pipeline ParserOptions::takePipeline() {
     if (!m_pipeline) {
         fmt::print(stderr, "Internal Error: Pipeline cannot be acceesd if not set\n");
         exit(1);
     }
-    std::optional<Pipeline> ret = std::nullopt;
+    std::optional<Pipeline> ret {};
     m_pipeline.swap(ret);
     return std::move(ret.value());
 }
 
 void ParserOptions::clearPipeline() {
-    m_pipeline = std::nullopt;
+    m_pipeline.reset();
 }
