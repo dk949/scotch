3c2bf73b127730e19f316a52cecdec85c594b2d4
dk949 <56653556+dk949@users.noreply.github.com>
2022-07-31 22:40:16 +0300

    build: added output to grammar generation
    This way it can be seen as a step in the build process

--- grammargen/source/app.d
+++ grammargen/source/app.d
@@ -3,5 +3,7 @@ import std.file;
 import pegged.grammar;
 
 void main() {
+    write("Writing generated grammar........");
     asModule("scot", "grammar/scot", std.file.readText("grammargen/views/" ~ "scot.pegd"));
+    writeln("complete.");
 }
