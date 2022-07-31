e335b03cf757e0c8b9e00ba1aaafdd8a4f672ebd
dk949 <56653556+dk949@users.noreply.github.com>
2022-08-01 01:01:19 +0300

    err: simplified error handling woth castSwitch

--- source/errors.d
+++ source/errors.d
@@ -1,54 +1,79 @@
 module errors;
 import logger;
 import utils;
+import std.exception;
+
+private enum : int {
+    Unhandled = 4,
+    Internal,
+}
 
 class ArgumentException : Exception {
-    @nogc @safe nothrow
-    this(string msg) {
-        super(msg);
-    }
+    mixin basicExceptionCtors;
 }
 
 class CliException : Exception {
-    @nogc @safe nothrow
-    this(string msg) {
-        super(msg);
-    }
+    mixin basicExceptionCtors;
 }
 
 class ExitException : Exception {
     mixin propGet!(int, "exitCode");
 
-    @nogc @safe nothrow
-    this(int exitCode, string msg = null) {
-        super(msg);
+    @nogc @safe nothrow pure
+    this(int exitCode,
+        string msg = null,
+        string file = __FILE__,
+        size_t line = __LINE__,
+        Throwable next = null) {
+        super(msg, file, line);
         m_exitCode = exitCode;
     }
 }
 
+int handleExitException(const Logger log, ExitException e) {
+    if (e.exitCode == 0)
+        log.infoln(e.message);
+    else
+        log.error(e.message);
+    return e.exitCode;
+}
+
 class InternalError : Error {
-    @nogc @safe nothrow
-    this(string msg) {
-        super(msg);
+    @safe pure nothrow
+    this(string msg,
+        string file = __FILE__,
+        size_t line = __LINE__,
+        Throwable next = null) {
+        super("Internal Error: " ~ msg, file, line, next);
     }
 }
 
 class FrontendException : Exception {
-    @nogc @safe nothrow
-    this(string msg) {
-        super(msg);
-    }
-
+    mixin basicExceptionCtors;
 }
 
-int handleError(const Logger log, const Exception err) {
-    if (const exit = cast(ExitException) err) {
-        if (exit.exitCode == 0)
-            log.infoln(exit.message);
-        else
-            log.error(exit.message);
-        return exit.exitCode;
+int handleNullException(const Logger log) {
+    log.error("Internal Error: received null exception");
+    return Internal;
+}
+
+int handleUnhandled(const Logger log, Exception e) {
+    log.error(typeid(e), ": ", e.message);
+    return Unhandled;
+}
+
+int handleError(const Logger log, Exception err) {
+    import std.algorithm.comparison;
+    import std.functional;
+
+    try
+        return err.castSwitch!(
+            partial!(handleExitException, log),
+            partial!(handleNullException, log),
+            partial!(handleUnhandled, log),
+        );
+    catch (Exception e) {
+        log.error("Internal Error: Unhandled exception: ", typeid(e), e.message);
+        return Internal;
     }
-    log.error(err.message);
-    return 1;
 }
