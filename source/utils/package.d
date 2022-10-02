cd0c9b4d7f35b4a6a45c990b2ee3087f77949c42
dk949 <56653556+dk949@users.noreply.github.com>
2022-10-02 15:11:27 +0100

    utils: added a few more utilities
    * append: combines several Appender.put operations unto one
    * declval now returns by ref
    * notImplemented: assert 0 and print that function is not yet
      implemented.
    * unreachable: assert 0 and print that the state is unreachable
    * placeholder: in debug mode, print the function name and continue. In
      release - act like notImplemented.
    * assertNotNull: assert that the value passed to it is not null and
      return the value
    * ident: a delegate returning it's input unchanged
    * isNullAssignable: trait which is true for type which can be assigned
      to null (used in assertNotNull). Unit test provided.

--- source/utils/package.d
+++ source/utils/package.d
@@ -1,6 +1,19 @@
 module utils;
 
+@safe:
+import std.traits;
+import std.array;
+
 public import utils.stack;
+public import utils.set;
+
+void append(T, Args...)(ref T a, auto ref Args args)
+if (isInstanceOf!(Appender, T))
+{
+    foreach (arg; args) {
+        a.put(arg);
+    }
+}
 
 /**
  * Declares a private member variable `m_name` and a public memebr function
@@ -38,18 +51,93 @@ mixin template propGet(alias T, string name) {
 }
 
 /// Works like C++ std::declval.
-T declval(T)();
+ref T declval(T)();
 
-/// Not implemented
-void notImplemented(string msg = null) {
-    assert(0, "Not implemented" ~ (msg ? " " ~ msg : ""));
+/**
+ * This is a valid path for the application, but it is not yet available
+ *
+ * Like assert(0), but with a nicer message and a clear intent
+ */
+noreturn notImplemented(string func = __FUNCTION__)(string msg = null) {
+    assert(0, "n\n\n" ~ func ~ ": not implemented" ~ (msg ? ": " ~ msg : "") ~ "\n\n\n");
 }
 
-/// unreachable
-void unreachable(string msg = null) {
-    assert(0, "Unreachable" ~ (msg ? " " ~ msg : ""));
+/**
+ * This is an invalid path for the application.
+ *
+ * Like assert(0), but with a nicer message and a clear intent
+ */
+noreturn unreachable(string func = __FUNCTION__)(string msg = null) {
+    assert(0, "\n\n\n" ~ func ~ ": unreachable" ~ (msg ? ": " ~ msg : "") ~ "\n\n\n");
 }
 
+/// Like notImplemented, but in debug mode will print a message instead of crashing the program
+void placeholder(string func = __FUNCTION__)(string msg = null) {
+    debug {
+        import std.stdio;
+
+        writeln("[PLACEHOLDER] ", func, (msg ? ": " ~ msg : ""));
+    } else {
+        notImplemented!(func)(msg);
+    }
+
+}
+
+/// Like a regular assert, but makes the message more prominent
+void assertF(string func = __FUNCTION__)(bool b, string msg = null) {
+    assert(b, "n\n\n" ~ func ~ ": assertion failed" ~ (msg ? ": " ~ msg : "") ~ "\n\n\n");
+}
+
+T assertNotNull(T, string func = __FUNCTION__)(auto ref T val)
+if (isNullAssignable!T) {
+    assertF!func(val !is null);
+    return val;
+}
+
+/// Creates a delegate which returns it's input. Useful for SumType
 template ident(T) {
     T delegate(T) ident = (T t) => t;
+}
+
+enum isNullAssignable(T) = __traits(compiles, declval!T = null);
+
+/// isNullAssignable
+unittest {
+    // dfmt off
+
+    // True for:
+    // Arrays: pinters, static, dynamic, associative (also string)
+    static assert(isNullAssignable!(int*));
+    static assert(isNullAssignable!(int[1]));
+    static assert(isNullAssignable!(int[]));
+    static assert(isNullAssignable!(int[string]));
+    static assert(isNullAssignable!string);
+
+    // Functions: function pointer, delegate
+    static assert(isNullAssignable!(void function()));
+    static assert(isNullAssignable!(void delegate()));
+
+    // Classes
+    class C {}
+    static assert(isNullAssignable!C);
+
+    // Null
+    static assert(isNullAssignable!(typeof(null)));
+
+    // False for:
+    // Scalars
+    static assert(!isNullAssignable!int);
+
+    // Structs and unions
+    struct S {}
+    union U {}
+    static assert(!isNullAssignable!S);
+    static assert(!isNullAssignable!U);
+
+    // SIMD vectors
+    static assert(!isNullAssignable!(__vector(float[4])));
+
+    // Void
+    static assert(!isNullAssignable!void);
+    // dfmt on
 }
